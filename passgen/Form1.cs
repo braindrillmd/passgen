76987a48c70c54c9185ac1c01db32ca442067690
braindrillmd <braindrillmd@gmail.com>
2016-03-14 12:58:01 +0300

    Class 'PassGenState' added
    Added program state reprisentation

--- passgen/Form1.cs
+++ passgen/Form1.cs
@@ -32,4 +32,139 @@ namespace passgen
 
         }
     }
+
+    class PassGenState
+    {
+        private bool digits;
+        private bool upperCaseLetters;
+        private bool lowerCaseLetters;
+        private bool specialCharacters;
+        private bool memorizable;
+        private bool multiple;
+
+        public PassGenState()
+        {
+            digits = false;
+            upperCaseLetters = false;
+            lowerCaseLetters = false;
+            specialCharacters = false;
+            memorizable = false;
+            multiple = false;
+        }
+
+        public PassGenState useDigits()
+        {
+            digits = true;
+
+            return this;
+        }
+
+        public PassGenState useUpperCaseLetters()
+        {
+            upperCaseLetters = true;
+
+            return this;
+        }
+
+        public PassGenState useLowerCaseLetters()
+        {
+            lowerCaseLetters = true;
+
+            return this;
+        }
+
+        public PassGenState useSpecialCharacters()
+        {
+            specialCharacters = true;
+
+            return this;
+        }
+
+        public PassGenState setMemorizableMode()
+        {
+            memorizable = true;
+
+            return this;
+        }
+
+        public PassGenState generateMultiple()
+        {
+            multiple = true;
+
+            return this;
+        }
+
+        public PassGenState useNotDigits()
+        {
+            digits = false;
+
+            return this;
+        }
+
+        public PassGenState useNotUpperCaseLetters()
+        {
+            upperCaseLetters = false;
+
+            return this;
+        }
+
+        public PassGenState useNotLowerCaseLetters()
+        {
+            lowerCaseLetters = false;
+
+            return this;
+        }
+
+        public PassGenState useNotSpecialCharacters()
+        {
+            specialCharacters = false;
+
+            return this;
+        }
+
+        public PassGenState unsetMemorizableMode()
+        {
+            memorizable = false;
+
+            return this;
+        }
+
+        public PassGenState generateNotMultiple()
+        {
+            multiple = false;
+
+            return this;
+        }
+
+        public bool digitsUsed()
+        {
+            return digits;
+        }
+
+        public bool lowerCaseLettersUsed()
+        {
+            return lowerCaseLetters;
+        }
+
+        public bool upperCaseLettersUsed()
+        {
+            return upperCaseLetters;
+        }
+
+        public bool specialCharactersUsed()
+        {
+            return specialCharacters;
+        }
+
+        public bool memorizableModSet()
+        {
+            return memorizable;
+        }
+
+        public bool isMultiple()
+        {
+            return multiple;
+        }
+
+    }
 }
